f032572abb42f6be70fcf244a5a95989b428803c
Jonah Siekmann <siekmanj@oregonstate.edu>
2022-04-16 14:01:47 -0700

    got forward kinematics working, need to do space and body jacobian

--- rot.py
+++ rot.py
@@ -27,14 +27,6 @@ def R_z(o):
     ])
     return np.around(R, decimals=3)
 
-# Create homogenous transformation matrix from SO3 and translation vector
-def T(R, p):
-    t = np.zeros((4,4))
-    t[:3,:3] = R
-    t[:3,3] = np.array(p).T
-    t[3] = [0, 0, 0, 1]
-    return np.around(t, decimals=3)
-
 # Create skew symmetric (so3) matrix from 3d vector v
 def so3(v):
     assert len(v) == 3
@@ -44,21 +36,16 @@ def so3(v):
     w[1][2] = -v[0]
     return w - w.T
 
-def twist(q, s, theta, pitch):
-    v = np.zeros(6)
-    v[:3] = np.array(s) * theta
-    v[3:] = np.cross(-np.array(s) * theta, q) + pitch * np.array(s) * theta
-
-    if np.sum(np.abs(v[:3])) > 1e-6:
-        v /= np.linalg.norm(v[:3])
-    else:
-        raise NotImplementedError(q,s,theta,pitch)
-    return v
+# Create homogenous transformation matrix from SO3 and translation vector
+def T(R, p):
+    t = np.zeros((4,4))
+    t[:3,:3] = R
+    t[:3,3] = np.array(p).T
+    t[3] = [0, 0, 0, 1]
+    return np.around(t, decimals=3)
 
 # Create SE3 from a twist and angular displacement
-def SE3(twist, theta):
-    w, v = twist[:3], twist[3:]
-
+def SE3(w, v, theta):
     w_skew = so3(w)
 
     e_twist_t = np.zeros((4,4))
@@ -69,7 +56,74 @@ def SE3(twist, theta):
     e_twist_t[:3, 3] = (np.eye(3) * theta + (1 - np.cos(theta)) * w_skew + \
                        (theta - np.sin(theta)) * (w_skew @ w_skew)) @ v
     e_twist_t[3, 3] = 1
-    return np.around(e_twist_t, decimals=3)
+    #return np.around(e_twist_t, decimals=3)
+    return e_twist_t
+
+if False:
+    # Problem 1 b
+    M = T(np.eye(3), [0, 2, 1])
+
+    theta1 = 0
+    theta2 = np.pi/2
+    theta3 = -np.pi/2
+    theta4 = 1
+
+    eS1 = SE3([0, 0, 1], [0, 0, 0], theta1)
+    eS2 = SE3([0, 0, 1], [1, 0, 0], theta2)
+    eS3 = SE3([0, 0, 1], [2, 0, 0], theta3)
+    eS4 = SE3([0, 0, 0], [0, 0, 1], theta4)
+
+    print('Space screw result:')
+    S = eS1 @ eS2 @ eS3 @ eS4 @ M
+    print(S)
+
+    eB1 = SE3([0, 0, 1], [-2, 0, 0,], theta1)
+    eB2 = SE3([0, 0, 1], [-1, 0, 0,], theta2)
+    eB3 = SE3([0, 0, 1], [0, 0, 0,], theta3)
+    eB4 = SE3([0, 0, 0], [0, 0, 1,], theta4)
+
+    print('Body screw result:')
+    B = M @ eB1 @ eB2 @ eB3 @ eB4
+    print(B)
+
+if True:
+    # Problem 2
+    M = T(R_x(np.pi/2) @ R_y(np.pi), [0, 3, 2])
+    print(M)
+
+    theta1 = np.pi/2
+    theta2 = np.pi/2
+    theta3 = 1
+
+    eS1 = SE3([0, 0, 1], [0, 0, 0], theta1)
+    eS2 = SE3([1, 0, 0], [0, 2, 0], theta2)
+    eS3 = SE3([0, 0, 0], [0, 1, 0], theta3)
+
+    print('Space screw result:')
+    S = eS1 @ eS2 @ eS3 @ M
+    print(np.around(S))
+
+    eB1 = SE3([0, 1, 0], [3, 0, 0,], theta1)
+    eB2 = SE3([-1, 0, 0], [0, 3, 0,], theta2)
+    eB3 = SE3([0, 0, 0], [0, 0, 1], theta3)
+
+    print('Body screw result:')
+    B = M @ eB1 @ eB2 @ eB3
+    print(np.around(B))
+
+
+exit(0)
+
+def screw(q, s, theta, pitch):
+    w = np.array(s) * theta
+    v = np.cross(-np.array(s) * theta, q) + pitch * np.array(s) * theta
+
+    if np.sum(np.abs(v[:3])) > 1e-6:
+        v /= np.linalg.norm(w[:3])
+        w /= np.linalg.norm(w[:3])
+    else:
+        raise NotImplementedError(q,s,theta,pitch)
+    return w, v
 
 # Problem 3.24
 t = 4
@@ -77,22 +131,22 @@ t = 4
 q1 = np.zeros(3)
 s1 = [0, 0, 1] # Rotate about z
 theta1 = np.pi/4
-S1 = twist(q1, s1, theta1, 2)
-e_S1 = SE3(S1, t*theta1)
+S1_w, S1_v = screw(q1, s1, theta1, 2)
+e_S1 = SE3(S1_w, S1_v, t*theta1)
 print(e_S1)
 
 q2 = np.array([0, 0, 10])
 s2 = [0, 1, 0]
 theta2 = np.pi/8
-S2 = twist(q2, s2, theta2, 0)
-e_S2 = SE3(S2, t*theta2)
+S2_w, S2_v = screw(q2, s2, theta2, 0)
+e_S2 = SE3(S2_2, S2_v, t*theta2)
 print(e_S2)
 
 q3 = np.array([0, 5, 5])
 s3 = [1, 0, 0]
 theta3 = np.pi/4
-S3 = twist(q3, s3, theta3, 0)
-e_S3 = SE3(S3, t*theta3)
+S3_w, S3_v = screw(q3, s3, theta3, 0)
+e_S3 = SE3(S3_w, S3_v, t*theta3)
 print(e_S3)
 
 T_sb = T(R_z(np.pi/2), [0, 8, 5])
@@ -100,32 +154,3 @@ print(T_sb)
 
 T_sb_prime = e_S1 @ e_S2 @ e_S3 @ T_sb
 print('final:', T_sb_prime)
-
-
-#Ra = R_z(np.pi/2) @ R_x(np.pi/2)
-#Ra2 = R_x(np.pi/2) @ R_z(np.pi/2)
-#
-#Rb = R_x(-np.pi/2)
-#
-#Rab_dumb = R_z(-np.pi/2) @ R_x(np.pi) @ Ra
-#
-#Ra = Ra2
-#x = [1, 0, 0]
-#assert np.sum(Ra @ x - [0, 0, 1]) < 1e-4
-#
-#x = [0, 0, 1]
-#assert np.sum(Ra @ x - [0, -1, 0]) < 1e-4
-#
-#x = [0, 1, 0]
-#assert np.sum(Ra @ x - [-1, 0, 0]) < 1e-4
-#
-#x = [1, 0, 0]
-#assert np.sum(Ra.T @ x - [0, -1, 0]) < 1e-4
-#
-#x = [0, 1, 0]
-#assert np.sum(Ra.T @ x - [0, 0, -1]) < 1e-4
-#
-#x = [0, 0, 1]
-#assert np.sum(Ra.T @ x - [1, 0, 0]) < 1e-4
-#
-#print(np.linalg.det(Ra), np.linalg.det(Rb))
